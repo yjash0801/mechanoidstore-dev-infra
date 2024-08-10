51932489f03a581b2345051f2b7cb33df4e7ee8f
yjash0801 <y.jashwanth5668@gmail.com>
2024-08-10 14:53:33 -0400

    Changed the VPC cidr

--- 01-vpc/variables.tf
+++ 01-vpc/variables.tf
@@ -23,14 +23,17 @@ variable "environment" {
 }
 
 variable "public_subnets_cidr" {
+    type = list
     default = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets_cidr" {
+    type = list
     default = ["10.0.11.0/24", "10.0.12.0/24"]
 }
 
 variable "database_subnets_cidr" {
+    type = list
     default = ["10.0.21.0/24", "10.0.22.0/24"]
 }
 
